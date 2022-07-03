c6586be7909a719c38ce18c134f7da7716e59887
Ryan <ryanathomas013@gmail.com>
2022-07-03 15:32:18 +0100

    added popup, win condition and computer moves

--- rps.js
+++ rps.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+document.getElementById("dismiss-popup-btn").disabled = true;
 function play(playerPick)
 {
     computerPick = computer()
@@ -15,25 +16,44 @@ function play(playerPick)
     document.getElementById("winner").textContent= CFL(winnerDecider(playerPick, computerPick));
     document.getElementById("playerDecision").textContent=CFL(playerPick);
     document.getElementById("computerDecision").textContent=CFL(computerPick);
-
     document.getElementById("playerS").textContent=playerScore;
     document.getElementById("computerS").textContent=computerScore;
+    if(playerScore == 5)
+    {
+        finalWinner("player")
+    }
+    if(computerScore == 5)
+    {
+        finalWinner("computer")
+    }
 }
 
 function computer()
 {
     number = randomInteger(1, 3)
+    let rock = document.getElementById("Crock")
+    let paper = document.getElementById("Cpaper")
+    let scissors = document.getElementById("Cscissors")
     if(number === 1)
     {
         answer = "rock"
+        rock.classList.add("computerHover")
+        paper.classList.remove("computerHover")
+        scissors.classList.remove("computerHover")
     }
     else if(number === 2)
     {
         answer = "paper"
+        rock.classList.remove("computerHover")
+        paper.classList.add("computerHover")
+        scissors.classList.remove("computerHover")
     }
     else if(number === 3)
     {
         answer = "scissors"
+        rock.classList.remove("computerHover")
+        paper.classList.remove("computerHover")
+        scissors.classList.add("computerHover")
     }
     return answer
 }
@@ -126,6 +146,24 @@ function winner(winner, player, computer)
     }
 }
 
+function finalWinner(winner)
+{
+    if(winner == "computer")
+    {
+        document.getElementById("description").textContent = "Unfortunately you did not win! Restart and try again!"
+        document.getElementById("title").textContent = "You lost!"
+        document.getElementById("image").src="images/sad.png"
+    }
+    if(winner == "player")
+    {
+        document.getElementById("description").textContent = "Congrats! You beat the computer! Restart to try your luck again!"
+        document.getElementById("title").textContent = "You won!"
+        document.getElementById("image").src="images/happy.png"
+    }
+    document.getElementById("dismiss-popup-btn").disabled = false;
+    document.getElementsByClassName("popup")[0].classList.add("active");
+}
+
 
 function convertNumtoString(number)
 {
@@ -156,4 +194,24 @@ const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
     play("rock")
   });
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    play("paper")
+});
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    play("scissors")
+});
 
+  
+  document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
+    document.getElementsByClassName("popup")[0].classList.remove("active");
+    playerScore = 0
+    computerScore = 0
+    document.getElementById("winner").textContent= "";
+    document.getElementById("playerDecision").textContent="";
+    document.getElementById("computerDecision").textContent="";
+    document.getElementById("playerS").textContent=0;
+    document.getElementById("computerS").textContent=0;
+    document.getElementById("dismiss-popup-btn").disabled = true;
+  });
